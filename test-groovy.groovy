1da1e8850f3b93217ef2a72b58c0730f55e1d731
Kanatbek Sultanov <Kanatbek.Sultanov@us.mcd.com>
2019-08-14 22:18:47 -0500

    added Jenkinsfile

---
+++ test-groovy.groovy
@@ -0,0 +1,30 @@
+#! groovy
+
+
+pipeline {
+    agent {
+    }
+    environment {
+        BUILD_VERSION = nextVersionFromGit(scope)
+    }
+    stages {
+        stage('Install'){
+            echo "Installing ..."
+        }
+        stage('Test'){
+            echo "Testing ..."
+        }
+        stage('Deploying'){
+            echo "Deploying ..."
+        }
+    }
+}
+
+// Pull existing latest tags and increments by 1 tags for latest commint and pushes to git.
+// Uses same tag for build release
+
+def nextVersionFromGit(scope){
+    def latestVersion = sh returnStdout: true, script: 'git describe --tags "$(git rev-list --tags=*.*.* --max-count=1 2> /dev/null)" 2> /dev/null || echo 0.0.0'
+    def(major, minor, patch) = latestVersion.tokenize('.').collect { it.toInteger() }
+    latestVersion
+}
